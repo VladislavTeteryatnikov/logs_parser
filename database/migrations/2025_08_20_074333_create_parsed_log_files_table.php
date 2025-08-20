f05a423403f9190c363f6267c2103dd33474a3c1
Vladislav Teteryatnikov <vladislav.teteryatnikov@gmail.com>
2025-08-20 10:45:17 +0300

    миграция для таблицы с файлами, которые были распаршены

---
+++ database/migrations/2025_08_20_074333_create_parsed_log_files_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parsed_log_files', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_name');
+            $table->string('file_hash', 32);
+            $table->timestamp('parsed_at');
+
+            $table->unique('file_hash');
+            $table->index('filename');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('parsed_log_files');
+    }
+};
